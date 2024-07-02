b86af4b812c0a7dce9d9b6a085361352c8907428
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-02 19:50:39 -0400

    modified the expert prompt to not only show technical details.

--- prompts.py
+++ prompts.py
@@ -4,7 +4,7 @@ FIVE_YEAR_OLD_PROMPT = "Explain {topic} in {domain} to me like I am a 5 year old
 
 UNDERGRAD_PROMPT = "Explain {topic} in {domain} to me like I am an undergrad student. Provide a simple explanation, 2 use cases, and 2 advantages and disadvantages if they exist."
 
-EXPERT_PROMPT = "Explain {topic} and its architecture in {domain} to me like I am a subject matter expert. Provide a detailed technical explanation with step-by-step procedure in 8 steps."
+EXPERT_PROMPT = "Explain {topic} and its process in {domain} to me like I am a subject matter expert. Provide a detailed explanation with step-by-step procedure in 8 steps."
 
 def get_prompt(level, topic, domain):
     if level == "5 year old":
