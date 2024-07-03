a0393091ba02f296d5151451994e739c3f774fea
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-03 01:36:45 -0400

    prompts and formatting adjusted

--- prompts.py
+++ prompts.py
@@ -2,9 +2,9 @@
 
 FIVE_YEAR_OLD_PROMPT = "Explain {topic} in {domain} to me like I am a 5 year old."
 
-UNDERGRAD_PROMPT = "Explain {topic} in {domain} to me like I am an undergrad student. Provide a simple explanation, 2 use cases, and 2 advantages and disadvantages if they exist."
+UNDERGRAD_PROMPT = "Explain {topic} in {domain} to me like I am an undergrad student. Provide a simple explanation, 2 use cases, and 2 advantages and disadvantages if they exist. Drop starting and closing remarks."
 
-EXPERT_PROMPT = "Explain {topic} and its process in {domain} to me like I am a subject matter expert. Provide a detailed explanation with step-by-step procedure in 8 steps."
+EXPERT_PROMPT = "Explain {topic} and its process in {domain} to me like I am a subject matter expert. Provide a detailed explanation with step-by-step procedure in 8 steps. Drop starting and closing remarks."
 
 def get_prompt(level, topic, domain):
     if level == "5 year old":
