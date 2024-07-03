9691354aecd3b1f45dc64691a97153297e5f3beb
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-03 09:45:15 -0400

    UI: Input section final

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("input[name='topic']").focus();
+
+    // Social media button hover effect
+    const fabContainer = document.querySelector('.fab-container');
+    const fabOptions = document.querySelector('.fab-options');
+
+    fabContainer.addEventListener('mouseover', function () {
+        fabOptions.style.display = 'flex';
+    });
+
+    fabContainer.addEventListener('mouseout', function () {
+        fabOptions.style.display = 'none';
+    });
+
+    fabOptions.addEventListener('mouseover', function () {
+        fabOptions.style.display = 'flex';
+    });
+
+    fabOptions.addEventListener('mouseout', function () {
+        fabOptions.style.display = 'none';
+    });
 });
-
-{/* <script src="{{ url_for('static', filename='js/script.js') }}"></script> */ }
-
-// background: rgb(245, 245, 245);
