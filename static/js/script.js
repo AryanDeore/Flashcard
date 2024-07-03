7ddc53e30f6fa5507a281425895bce9a859e7b46
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-02 22:09:04 -0400

    working

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("input[name='topic']").focus();
 });
+
+{/* <script src="{{ url_for('static', filename='js/script.js') }}"></script> */ }
+
+// background: rgb(245, 245, 245);
