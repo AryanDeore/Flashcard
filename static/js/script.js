720284e4f80282f55b0f42ebe95031f70447d045
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-04 03:31:21 -0400

    cleaned js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("input[name='topic']").focus();
 
-    // Social media button hover effect
-    const fabContainer = document.querySelector('.fab-container');
-    const fabOptions = document.querySelector('.fab-options');
-
-    // Show options on mouseover and keep them visible for a while
-    fabContainer.addEventListener('mouseover', function () {
-        fabOptions.style.display = 'flex';
-    });
-
-    // Hide options after a delay on mouseout
-    fabContainer.addEventListener('mouseout', function () {
-        setTimeout(function () {
-            fabOptions.style.display = 'none';
-        }, 500); // Adjust delay as needed
-    });
-
-    // Ensure options stay visible when hovering over them
-    fabOptions.addEventListener('mouseover', function () {
-        fabOptions.style.display = 'flex';
-    });
-
-    // Hide options after a delay when mouse leaves the options
-    fabOptions.addEventListener('mouseout', function () {
-        setTimeout(function () {
-            fabOptions.style.display = 'none';
-        }, 100000); // Adjust delay as needed
-    });
 });
