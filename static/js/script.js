c67e1f7f34e89324010e661e50ebaf9196f69dae
Aryan Deore <36997710+Aryan120798@users.noreply.github.com>
2024-07-18 17:22:00 -0400

    added spinner

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('explain-form');
+    const spinner = document.getElementById('spinner');
+
     document.querySelector("input[name='topic']").focus();
 
     if (performance.navigation.type === 1) {
         // Page was reloaded
         window.location.href = '/refresh';
     }
+
+    form.addEventListener('submit', function (event) {
+        spinner.style.visibility = 'visible';
+    });
+
+    // Hide spinner when page is fully loaded
+    window.addEventListener('load', function () {
+        spinner.style.visibility = 'hidden';
+    });
 });
